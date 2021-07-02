df91fdcaea0d0e5dc03e4ac27b29c26bd24e46ef
Janeway42 <cpopa@student.codam.nl>
2021-07-02 20:03:30 +0200

    progress

--- utils/utils_sort.c
+++ utils/utils_sort.c
@@ -6,23 +6,35 @@
 /*   By: janeway <janeway@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/19 17:39:44 by janeway       #+#    #+#                 */
-/*   Updated: 2021/06/30 10:53:55 by janeway       ########   odam.nl         */
+/*   Updated: 2021/07/02 19:19:18 by janeway       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../src/push_swap.h"
 
-void move_elem(t_stack **x, int number)
+static int find_half(t_stack *x)
 {
-	int pos;
 	int size;
+	int half;
 
-	size = iterate(*x);
+	size = iterate(x);
+	if (size % 2 == 0)
+		half = size / 2;
+	else
+		half = size / 2 + 1;
+	return (half);
+}
 
+void move_elem_a(t_stack **x, int number)
+{
+	int half;
+	int pos;
+
+	half = find_half(*x);
 	pos = find_position(*x, number);
 	if (pos == 1)
 		return ;
-	if (pos <= size / 2)
+	if (pos <= half)
 	{
 		while ( pos != 1)
 		{
@@ -30,7 +42,7 @@ void move_elem(t_stack **x, int number)
 			pos--;
 		}
 	}
-	if (pos > size / 2)
+	if (pos > half)
 	{
 		while (pos != iterate(*x) + 1)
 		{
@@ -39,6 +51,34 @@ void move_elem(t_stack **x, int number)
 		}
 	}
 }
+
+void move_elem_b(t_stack **x, int number)
+{
+	int half;
+	int pos;
+
+	half = find_half(*x);
+	pos = find_position(*x, number);
+	if (pos == 1)
+		return ;
+	if (pos <= half)
+	{
+		while ( pos != 1)
+		{
+			write_rb(x);
+			pos--;
+		}
+	}
+	if (pos > half)
+	{
+		while (pos != iterate(*x) + 1)
+		{
+			write_rrb(x);
+			pos++;
+		}
+	}
+}
+
 
 int find_min(t_stack *x)
 {
